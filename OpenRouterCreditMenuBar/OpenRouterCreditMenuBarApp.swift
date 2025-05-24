d7489b85398be6b97cc7952711689be83eb7cab5
Kittizz <62899732+kittizz@users.noreply.github.com>
2025-05-24 16:04:50 +0700

    Initial Commit

---
+++ OpenRouterCreditMenuBar/OpenRouterCreditMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenRouterCreditMenuBarApp.swift
+//  OpenRouterCreditMenuBar
+//
+//  Created by Kittithat Patepakorn on 24/5/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct OpenRouterCreditMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenRouterCreditMenuBarTests/OpenRouterCreditMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenRouterCreditMenuBarTests.swift
+//  OpenRouterCreditMenuBarTests
+//
+//  Created by Kittithat Patepakorn on 24/5/2568 BE.
+//
+
+import Testing
+@testable import OpenRouterCreditMenuBar
+
+struct OpenRouterCreditMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
